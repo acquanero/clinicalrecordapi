5654fc6905711ac5af7107367796fa742abeb33b
Pablo <pabloacquafresca@gmail.com>
2021-01-23 11:49:37 -0300

    rename route surgeryssupply to suplly (error of previous commit)

--- api/supplys/routes.js
+++ api/supplys/routes.js
@@ -37,9 +37,9 @@ router.get('/', isAuthenticated, async (req, res) => {
 
 //modify supply name
 
-router.put('/:surgerysupplyid', isAuthenticated, async (req, res) => {
+router.put('/:supplyid', isAuthenticated, async (req, res) => {
 
-    let id = req.params.surgerysupplyid;
+    let id = req.params.supplyid;
 
     const { name } = req.body;
 
